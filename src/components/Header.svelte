f0694f600bd911cdebf267b75c71ca6412876f0a
NickDrew <nicholas.drew@trussle.com>
2021-01-09 11:46:03 +0000

    plural and guests

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,21 +1,20 @@
-
 <style>
-    .header-outer{
+    .header-outer {
         background-image: url("/images/headerFence.jpg");
         background-size: cover;
         /* background-repeat:round; */
-        height:100px;
-        margin-bottom:20px;
+        height: 100px;
+        margin-bottom: 20px;
     }
     .header-text {
         color: red;
         font-size: 75px;
         font-weight: bold;
-        
+
         text-shadow: -3px 0 black, 0 3px black, 3px 0 black, 0 -3px black;
     }
 </style>
 
 <div class="header-outer">
-<div class="header-text">CONTAIN.    THE.    THING.</div>
+    <div class="header-text">CONTAIN. THE. THINGS.</div>
 </div>
